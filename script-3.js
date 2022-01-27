694c598e8df63c42045782b7e02804a73f135acc
Azamat <Azamat>
2022-01-27 21:21:51 +0600

    Task-1

---
+++ script-3.js
@@ -0,0 +1,6 @@
+// 3. Напишите несколько строк кода, выводящих на экран ваше имя и почтовый адрес.
+
+let name = 'Azamat'
+let email = 'azamat.kanatbekov.kk@gmail.com'
+alert(`Менин атым ${name},
+Менин почтам ${email}`)
